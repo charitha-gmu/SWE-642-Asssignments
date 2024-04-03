b548e6401a226969017cc662d03f7872e4d57ee8
charitha-meka <mekacharitha.19@gmail.com>
2024-04-02 20:27:37 -0400

    Initial BE changes

--- Assignment 3/survey-app/src/app/app.routes.ts
+++ Assignment 3/survey-app/src/app/app.routes.ts
@@ -1,3 +1,23 @@
-import { Routes } from '@angular/router';
+import { Route } from '@angular/router';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { StudentSurveyComponent } from './student-survey/student-survey.component';
+import { SurveyListComponent } from './survey-list/survey-list.component';
 
-export const routes: Routes = [];
+export const routes: Route[] = [
+  {
+    path: '',
+    component: WelcomeComponent,
+    title: 'Welcome',
+  },
+  {
+    path: 'survey',
+    component: StudentSurveyComponent,
+    title: 'Survey',
+  },
+  {
+    path: 'all-surveys',
+    component: SurveyListComponent,
+    title: 'All Surveys',
+  },
+  // ...other routes
+];
